b7ab42dfcf7f659913fad6684da69ef3a64941aa
jaehoonmandev <wido1593@gmail.com>
2024-03-20 11:37:42 +0900

    제거를 위한 설정

---
+++ src/test/java/com/jaehoonman/guestbookspringboot/config/JasyptConfigAESTest.java
@@ -0,0 +1,30 @@
+package com.jaehoonman.guestbookspringboot.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.junit.jupiter.api.Test;
+
+class JasyptConfigAESTest {
+
+    @Test
+    void stringEncryptor() {
+        String url = "db_url";
+        String username = "jaehoonmanProd";
+        String password = "Q!w2e3r41593";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "jaehoonmanProd";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        pbeEnc.setPassword(key);
+        pbeEnc.setIvGenerator(new RandomIvGenerator());
+        return pbeEnc.encrypt(value);
+    }
+
+}
