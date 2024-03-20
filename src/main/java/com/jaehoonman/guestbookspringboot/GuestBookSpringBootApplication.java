a200c21bda725cd18dd2a96c43c26aeb037b2c5d
jaehoonmandev <wido1593@gmail.com>
2024-03-20 12:42:39 +0900

    1. git action secret 설정 값 출력 테스트

--- src/main/java/com/jaehoonman/guestbookspringboot/GuestBookSpringBootApplication.java
+++ src/main/java/com/jaehoonman/guestbookspringboot/GuestBookSpringBootApplication.java
@@ -1,6 +1,8 @@
 package com.jaehoonman.guestbookspringboot;
 
 import com.jaehoonman.guestbookspringboot.controller.GuestBookController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +12,17 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableMongoAuditing // GuestBook Model에서 자동 생성 시간 저장을 위한.
 public class GuestBookSpringBootApplication {
 
+    @Value("${jasypt.encryptor.key}")
+    private String key;
+
     public static void main(String[] args) {
         SpringApplication.run(GuestBookSpringBootApplication.class, args);
     }
 
+
+    @PostConstruct
+    private void start() {
+        System.out.println("jasypt.encryptor.key = " + key);
+    }
 
 }
